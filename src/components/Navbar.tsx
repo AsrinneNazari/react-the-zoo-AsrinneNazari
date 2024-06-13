899f3a39f02a47d272926b4aed9067b2b3733f68
Asrinne Nazari <asse_89@hotmail.com>
2024-06-13 22:03:34 +0200

    added more styling in nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,11 +4,13 @@ import "./../styles/Navbar.scss"
 export const Navbar = () =>{
     return <>
     <div className="navBar">
-        
+        <div className="navBar-left">
         <div className="nav-icon"><img src="/public/img/giraffe-pic.png"/></div>
-        <h3>The Zoo</h3>
-    <NavLink to={"/home"}>Hem</NavLink>
+        <h3>The Zoo</h3></div>
+        <div className="navBar-right">
+            <NavLink to={"/home"}>Hem</NavLink>
     <NavLink to={"/animals"}>Dina Djur</NavLink>
+    </div>
     </div>
     
     </>
