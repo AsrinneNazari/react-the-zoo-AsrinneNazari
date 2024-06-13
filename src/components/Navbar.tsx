1e0bdb3d35f79f7e762eab4a36da08931a372f1c
Asrinne Nazari <asse_89@hotmail.com>
2024-06-13 14:43:02 +0200

    🧐 error help

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,7 @@ import { NavLink } from "react-router-dom"
 export const Navbar = () =>{
     return <>
     <div className="navBar">
+        <div className="nav-icon"><img src="/public/img/giraffe-pic.png"/></div>
     <NavLink to={"/home"}>Hem</NavLink>
     <NavLink to={"/animals"}>Dina Djur</NavLink>
     </div>
