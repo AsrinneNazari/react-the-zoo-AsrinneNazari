c5b98937621a1f897b44c3f91687cf99d356c9fb
Asrinne Nazari <asse_89@hotmail.com>
2024-06-18 13:44:52 +0200

    🔨 more styling

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -2,7 +2,7 @@ import "./../styles/NotFound.scss";
 export const NotFound = () => {
   const errorImg = "/public/img/error-icon.png";
   return (
-    <h3>
+    <h3 className="notFound">
       Oj något blev fel! <img src={errorImg} alt="error icon" />
     </h3>
   );
